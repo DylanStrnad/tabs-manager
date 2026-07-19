9d3dde0467ebef062c4f2d7f0f481f642074a214
Dylan Strnad <dstrnad1@kent.edu>
2026-07-19 14:55:17 -0400

    close inactive tabs

--- src/scripts/close-inactive.js
+++ src/scripts/close-inactive.js
@@ -1,1 +1,15 @@
 //close all inactive tabs
+async function closeInactiveTabs() {
+    const tabs = document.querySelector("#apply-all-windows").checked ? await chrome.tabs.query({}) : await chrome.tabs.query({currentWindow: true});
+    
+    const cutOffInMinutes = parseInt(document.querySelector('#inactive-time').value);
+    const cutOffTime = Date.now() - cutOffInMinutes * 60 * 1000;
+    for (const tab of tabs) {
+        if(tab.lastAccessed < cutOffTime){
+            chrome.tabs.remove(tab.id);
+        }
+    }
+
+}
+const closeInactiveButton = document.querySelector("#close-inactive");
+closeInactiveButton.addEventListener("click", closeInactiveTabs);
